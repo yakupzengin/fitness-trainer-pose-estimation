4723162efa336637a3dc158242cf0cdecd79da62
Yakup Zengin <118113891+yakupzengin@users.noreply.github.com>
2024-09-06 10:17:16 +0300

    add draw line with style :push_up.py

--- exercises/push_up.py
+++ exercises/push_up.py
@@ -1,6 +1,6 @@
 import cv2
-from pose_estimation.angle_calculation import calculate_angle
 import time
+from pose_estimation.angle_calculation import calculate_angle
 
 class PushUp:
     def __init__(self):
@@ -10,12 +10,12 @@ class PushUp:
         self.angle_threshold_down = 70  # Lower threshold for 'down' stage
         self.last_counter_update = time.time()  # Track the time of the last counter update
 
-    def calculate_shoulder_elbow_hip_angle(self, shoulder, elbow, hip):
-        """Calculate the angle between shoulder, elbow, and hip."""
-        return calculate_angle(shoulder, elbow, hip)
+    def calculate_shoulder_elbow_wrist_angle(self, shoulder, elbow, wrist):
+        """Calculate the angle between shoulder, elbow, and wrist."""
+        return calculate_angle(shoulder, elbow, wrist)
 
     def track_push_up(self, landmarks, frame):
-        # Right side landmarks (shoulder, elbow, hip)
+        # Right side landmarks (shoulder, elbow, wrist)
         shoulder_left = [int(landmarks[11].x * frame.shape[1]), int(landmarks[11].y * frame.shape[0])]
         elbow_left = [int(landmarks[13].x * frame.shape[1]), int(landmarks[13].y * frame.shape[0])]
         wrist_left = [int(landmarks[15].x * frame.shape[1]), int(landmarks[15].y * frame.shape[0])]
@@ -24,27 +24,32 @@ class PushUp:
         elbow_right = [int(landmarks[14].x * frame.shape[1]), int(landmarks[14].y * frame.shape[0])]
         wrist_right = [int(landmarks[16].x * frame.shape[1]), int(landmarks[16].y * frame.shape[0])]
 
+        # Calculate angles for push-up tracking
+        angle_left = self.calculate_shoulder_elbow_wrist_angle(shoulder_left, elbow_left, wrist_left)
+        angle_right = self.calculate_shoulder_elbow_wrist_angle(shoulder_right, elbow_right, wrist_right)
 
-        # Calculate the angle for the right side (shoulder, elbow, hip)
-        angle_left = self.calculate_shoulder_elbow_hip_angle(shoulder_left, elbow_left, wrist_left)
-        angle_right = self.calculate_shoulder_elbow_hip_angle(shoulder_right, elbow_right, wrist_right)
+        # Draw lines with improved style
+        self.draw_line_with_style(frame, shoulder_left, elbow_left, (0, 0, 255), 2)
+        self.draw_line_with_style(frame, elbow_left, wrist_left, (0, 0, 255), 2)
 
-        # Draw lines between landmarks and angle
-        cv2.line(frame, shoulder_left, elbow_left, (0, 255, 0), 2)  # Shoulder - Elbow
-        cv2.line(frame, elbow_left, wrist_left, (0, 255, 0), 2)  # Elbow - Wrist
+        self.draw_line_with_style(frame, shoulder_right, elbow_right, (102, 0, 0), 2)
+        self.draw_line_with_style(frame, elbow_right, wrist_right, (102, 0, 0), 2)
 
-        # draw lines between landmarks and angle for right side
-        cv2.line(frame, shoulder_right, elbow_right, (0, 255, 0), 2)  # Shoulder - Elbow
-        cv2.line(frame, elbow_right, wrist_right, (0, 255, 0), 2)  # Elbow - Wrist
+        # Draw circles to highlight key points
+        self.draw_circle(frame, shoulder_left, (0, 0, 255), 8)
+        self.draw_circle(frame, elbow_left, (0, 0, 255), 8)
+        self.draw_circle(frame, wrist_left, (0, 0, 255), 8)
 
-        # Draw angle text
-        angle_text_position = (elbow_left[0] + 10, elbow_left[1] - 10)
-        cv2.putText(frame, f'Angle: {int(angle_left)}', angle_text_position, cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+        self.draw_circle(frame, shoulder_right, (102, 0, 0), 8)
+        self.draw_circle(frame, elbow_right, (102, 0, 0), 8)
+        self.draw_circle(frame, wrist_right, (102, 0, 0), 8)
 
-        # draw angle text for right side
+        # Update angle text positions and display
+        angle_text_position_left = (elbow_left[0] + 10, elbow_left[1] - 10)
+        cv2.putText(frame, f'Angle: {int(angle_left)}', angle_text_position_left, cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+
         angle_text_position_right = (elbow_right[0] + 10, elbow_right[1] - 10)
-        cv2.putText(frame, f'Angle: {int(angle_left)}', angle_text_position_right, cv2.FONT_HERSHEY_SIMPLEX, 0.5,
-                    (255, 255, 255), 2)
+        cv2.putText(frame, f'Angle: {int(angle_right)}', angle_text_position_right, cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
 
         # Get current time
         current_time = time.time()
@@ -62,3 +67,11 @@ class PushUp:
                 self.last_counter_update = current_time
 
         return self.counter, angle_left, self.stage
+
+    def draw_line_with_style(self, frame, start_point, end_point, color, thickness):
+        """Draw a line with specified style."""
+        cv2.line(frame, start_point, end_point, color, thickness, lineType=cv2.LINE_AA)
+
+    def draw_circle(self, frame, center, color, radius):
+        """Draw a circle with specified style."""
+        cv2.circle(frame, center, radius, color, -1)  # -1 to fill the circle
